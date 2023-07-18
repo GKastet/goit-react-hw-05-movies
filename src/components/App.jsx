b0e90e5df4c28c34e513155b9c52371b7a18b5e6
GKastet <127600582+GKastet@users.noreply.github.com>
2023-07-18 13:59:43 +0200

    browser router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
